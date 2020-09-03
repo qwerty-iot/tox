6ccc7609a010b915b3369c6fb923589a80beb620
Jim Wert <jimwert@gmail.com>
2020-09-03 13:02:02 -0400

    Small lint fix.

--- byte.go
+++ byte.go
@@ -1,14 +1,13 @@
 package tox
 
-// ToBool converts bool, string, or byte arrays, if the conversion fails, it returns false.
+// ToByteArray converts bool, string, or byte arrays, if the conversion fails, it returns false.
 func ToByteArray(v interface{}) []byte {
 	switch v := v.(type) {
 	case bool:
 		if v {
 			return []byte{0x01}
-		} else {
-			return []byte{0x00}
 		}
+		return []byte{0x00}
 	case nil:
 		return nil
 	case string:
