f58bf064f4827188131699bdca6658e71a984d39
Jim Wert <jimwert@gmail.com>
2022-10-25 10:04:58 -0400

    added ToMapStringInterface()

--- map.go
+++ map.go
@@ -1,5 +1,7 @@
 package tox
 
+import "encoding/json"
+
 // ToMapStringString converts a generic map[string]interface{} to a map[string]string.
 func ToMapStringString(v interface{}) map[string]string {
 	switch v := v.(type) {
@@ -11,5 +13,29 @@ func ToMapStringString(v interface{}) map[string]string {
 		return ret
 	default:
 		return nil
+	}
+}
+
+func ToMapStringInterface(v interface{}) map[string]interface{} {
+	switch v := v.(type) {
+	case map[string]interface{}:
+		return v
+	case map[string]string:
+		ret := map[string]interface{}{}
+		for key, val := range v {
+			ret[key] = val
+		}
+		return ret
+	default:
+		b, err := json.Marshal(v)
+		if err != nil {
+			return nil
+		}
+		ret := map[string]interface{}{}
+		err = json.Unmarshal(b, &ret)
+		if err != nil {
+			return nil
+		}
+		return ret
 	}
 }
