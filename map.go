5aaebd310e19ce11be330e4e9a7b4751cbffcf78
Jim Wert <jimwert@gmail.com>
2023-04-26 09:30:47 -0400

    small fix for MapStringString()

--- map.go
+++ map.go
@@ -5,6 +5,8 @@ import "encoding/json"
 // ToMapStringString converts a generic map[string]interface{} to a map[string]string.
 func ToMapStringString(v interface{}) map[string]string {
 	switch v := v.(type) {
+	case map[string]string:
+		return v
 	case map[string]interface{}:
 		ret := map[string]string{}
 		for key, val := range v {
